a250ee0956ef66e2f0832cb2db7ef62f875b944c
Neel Patel <patelneel55@gmail.com>
2018-11-14 12:08:10 -0500

    Add initial kfold cross-validation implementation

---
+++ k-fold.py
@@ -0,0 +1,29 @@
+import numpy as np
+import linreg as lr
+
+def run(k, X, y):
+
+    (n,d) = X.shape
+    z = np.zeros((k, 1))
+    for i in range(k):
+        S = []
+        T = []
+        for j in range(n):
+            if j >= np.floor((n * i) / k) and j <= np.floor(((n * (i + 1)) / k) - 1):
+                T.append(j)
+            else
+                S.append(j)
+
+        subset = np.array(S)
+        theta = linreg(X[subset,:], y[subset])
+        val = 0
+
+        for t in T:
+            val += (y[t] - np.dot(theta.transpose(), X[t])) ** 2
+
+        if(len(T) > 0):
+            val = val / len(T)
+
+        z[i] = val
+
+    return z
